48a838d07a2b78fb7a979cf7292ef4af1ce91412
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-08-24 10:59:26 -0300

    Added humans walking on BombExplodeActivity.

--- src/TenSecondHero/Activities/GamePlay/BombExplodeActivity.cs
+++ src/TenSecondHero/Activities/GamePlay/BombExplodeActivity.cs
@@ -5,15 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using TenSecondHero.Entities;
+using TenSecondHero.Behaviors;
 
 namespace TenSecondHero.Activities.GamePlay
 {
     class BombExplodeActivity : GamePlayActivity
     {
         public BombExplodeActivity(Game game)
-            : base(game, "Content/maps/transform.tmx")
+            : base(game, "Content/maps/bomb_explode.tmx")
         {
-            
+            foreach (var ent in _entities.OfType<Entities.Object>().Where(e => e.Name == "Human"))
+            {
+                ent.Behaviors.Add(new WalkLeftRightBehavior(_levelMap, ent));
+            }
         }
     }
 }
---
+++ src/TenSecondHero/Behaviors/WalkLeftRightBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGameLib.Core.Entities;
+using MonoGameLib.Tiled;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TenSecondHero.Entities;
+
+namespace TenSecondHero.Behaviors
+{
+    class WalkLeftRightBehavior : Behavior
+    {
+        static int Id;
+
+        public Map Map { get; private set; }
+        float _walkDirection = 1;
+
+        float _minSpeed = 1f;
+        float _maxSpeed = 2;
+
+        new BaseEntity Entity { get { return (BaseEntity)base.Entity; } }
+
+        public WalkLeftRightBehavior(Map map, BaseEntity parent) : base(parent)
+        {
+            var rnd = new Random(Id++ + Environment.TickCount);
+            _walkDirection = _minSpeed + (float)(rnd.NextDouble()) * (_maxSpeed - _minSpeed);
+            if (rnd.Next(0, 2) == 0)
+                _walkDirection *= -1;
+            Map = map;
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            Entity.LastPosition = Entity.Position;
+            Entity.Position += new Vector2(_walkDirection, 0);
+            if (Map.IsOutsideBorders(Entity.BoundingBox))
+            {
+                _walkDirection *= -1;
+                Entity.Position = Entity.LastPosition + new Vector2(_walkDirection, 0);
+            }
+        }
+    }
+}
