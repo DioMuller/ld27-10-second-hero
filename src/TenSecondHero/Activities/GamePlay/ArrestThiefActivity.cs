d1d936ad8426203a8e7ff760002f40a658f687ac
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-08-24 17:12:20 -0300

    Added thief to ArrestThiefActivity.

--- src/TenSecondHero/Activities/GamePlay/ArrestThiefActivity.cs
+++ src/TenSecondHero/Activities/GamePlay/ArrestThiefActivity.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Xna.Framework;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using TenSecondHero.Entities;
 using TenSecondHero.Behaviors;
-using TenSecondHero.Core;
 
 namespace TenSecondHero.Activities.GamePlay
 {
@@ -18,7 +12,7 @@ namespace TenSecondHero.Activities.GamePlay
             foreach (var ent in _entities)
                 ent.CollidesWithMap = false;
 
-            foreach (var ent in _entities.OfType<Entities.Object>().Where(e => e.Name.Contains("Citizen")))
+            foreach (var ent in _entities.OfType<Entities.Object>())
                 ent.Behaviors.Add(new WalkLeftRightBehavior(_levelMap, ent));
         }
 
@@ -26,17 +20,8 @@ namespace TenSecondHero.Activities.GamePlay
         {
             base.Update(gameTime);
 
-            /*if (!_exploding && _entities.OfType<>().Count() <= 0)
-            {
+            if (_entities.OfType<Entities.Object>().Count(e => e.Name.Contains("Thief")) <= 0)
                 Exit(true);
-            }*/
-        }
-
-        public async override void OnTimeout()
-        {
-            //_exploding = true;
-            //await _entities.OfType<Bomb>().First().Explode();
-            base.OnTimeout();
         }
     }
 }
