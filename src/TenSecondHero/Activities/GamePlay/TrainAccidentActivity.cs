2fa77c24e6482725de630e570a08df12f2505250
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-08-24 19:59:07 -0300

    WIP: TrainAccidentActivity.

---
+++ src/TenSecondHero/Activities/GamePlay/TrainAccidentActivity.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System.Linq;
+using TenSecondHero.Behaviors;
+
+namespace TenSecondHero.Activities.GamePlay
+{
+    class TrainAccidentActivity : GamePlayActivity
+    {
+        public TrainAccidentActivity(MainGame game)
+            : base(game, "Content/maps/train_accident.tmx", "images/background_morningsky.png")
+        {
+            //foreach (var ent in _entities)
+            //    ent.CollidesWithMap = false;
+
+            //foreach (var ent in _entities.OfType<Entities.Object>())
+            //    ent.Behaviors.Add(new WalkLeftRightBehavior(_levelMap, ent));
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            //if (_entities.OfType<Entities.Object>().Count(e => e.Name.Contains("Thief")) <= 0)
+            //    Exit(true);
+        }
+    }
+}
