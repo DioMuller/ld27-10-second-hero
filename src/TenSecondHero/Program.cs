e6d3fa91f0223b75af4ccbf3038ff77e64b04f7d
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-08-23 23:52:47 -0300

    Running MonoGame project template.

---
+++ src/TenSecondHero/Program.cs
@@ -0,0 +1,36 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace TenSecondHero
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        static void SetupLibraries()
+        {
+            #region Include Libraries to path
+            string dllDir = @".\lib\" + (Environment.Is64BitProcess ? "x64" : "x86");
+            Environment.SetEnvironmentVariable("PATH", dllDir + ";" + Environment.GetEnvironmentVariable("PATH"));
+            #endregion
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            SetupLibraries();
+
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
