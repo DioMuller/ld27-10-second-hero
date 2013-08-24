62702a12f3c64266b4cc0c292eef1c1f5c702a52
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-08-24 14:36:15 -0300

    Added explosion to bomb on level timeout.

---
+++ src/TenSecondHero/Entities/Explosion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using MonoGameLib.Core.Entities;
+using MonoGameLib.Core.Input;
+using MonoGameLib.Core.Sprites;
+using TenSecondHero.Behaviors;
+
+namespace TenSecondHero.Entities
+{
+    class Explosion : Object
+    {
+        public Explosion()
+            : base("Explosion", new Vector2(32, 32))
+        {
+            Sprite.Animations.Add(new Animation("exploding", 0, 0, 1));
+            Sprite.ChangeAnimation("exploding");
+        }
+    }
+}
