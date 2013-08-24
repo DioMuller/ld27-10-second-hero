ce06ff81033bd1b3731826dcc52150e2850aa69b
Diogo Muller <diogo.muller@live.com>
2013-08-24 07:48:35 -0300

    Added Player entity, fixed sprites transparency, added font.

---
+++ src/TenSecondHero/Entities/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using MonoGameLib.Core.Entities;
+using MonoGameLib.Core.Sprites;
+
+namespace TenSecondHero.Entities
+{
+    class Player : Entity
+    {
+        public Player() : base()
+        {
+            //TODO: Add behaviors;
+
+            Sprite = new Sprite("sprites/10sechero.png", new Point(16, 48), 0);
+
+            Sprite.Animations.Add(new Animation("default", 0, 0, 0));
+            Sprite.ChangeAnimation(0);
+        }
+    }
+}
